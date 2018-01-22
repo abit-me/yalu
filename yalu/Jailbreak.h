8712aaeab01add2cabee5c90c8475f502146ffba
A <a@abit.me>
2018-01-22 15:22:35 +0800

    拿到root权限

---
+++ yalu/Jailbreak.h
@@ -0,0 +1,14 @@
+//
+//  Jailbreak.h
+//  yalu
+//
+//  Created by A on 2018/1/22.
+//  Copyright © 2018年 A. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//@interface Jailbreak : NSObject
+//
+//@end
+
---
+++ yalu/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  yalu
+//
+//  Created by A on 2018/1/22.
+//  Copyright © 2018年 A. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
